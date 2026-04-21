55d0bf40e2b7be1f248d5da104b1ebd5971910b7
AndrMenezes <andrefelipemaringa@gmail.com>
2026-04-21 17:41:33 +0100

    test inverse posterior in one and two dimension examples for ml-bart

--- tests/testthat/test-inverse_posterior.R
+++ tests/testthat/test-inverse_posterior.R
@@ -5,62 +5,82 @@ test_that("inverse posterior ML-BART univariate X", {
   devtools::load_all()
 
   # Path
-  time_id <- "2026-Apr-13-12:03:35"#format(Sys.time(), "%Y-%b-%d-%X")
-  path_local <- "./tests/testthat/inverse_posterior/ml_bart"
-  forests_dir <- file.path(path_local, time_id, "forests")
+  time_id <- format(Sys.time(), "%Y-%b-%d-%X")
+  path_local <- "./tests/testthat/inverse_posterior/one_dimension/ml_bart"
+  path_res <- file.path(path_local, time_id, "results")
+  forests_dir <- file.path(path_res, "forests")
   if (!dir.exists(forests_dir)) dir.create(forests_dir, recursive = TRUE)
 
-  n_sample <- 200L
+  set.seed(1212)
+  n_sample <- 300L
   d <- 3L
   n_trials <- 500L
   tmp <- sim_data_multinomial_bspline_curve(n = n_sample, d = d, n_trials = n_trials)
-  Y <- tmp$Y
-  X <- tmp$X
+
+  # Split the data
+  n_test <- 100L
+  id_test <- sample.int(n_sample, n_test)
+  Y_test <- tmp$Y[id_test, ]
+  X_test <- tmp$X[id_test, , drop = FALSE]
+  Y_train <- tmp$Y[id_test, ]
+  X_train <- tmp$X[id_test, , drop = FALSE]
 
   # Fit forward model
   NDPOST <- 5000L
-  # NSKIP <- 5000L
+  NSKIP <- 5000L
   NTREES <- 200L
-  # ml_bart <- zanicc(Y = Y, X_count = X, model = "mult_bart", ntrees_theta = NTREES,
-  #                   ndpost = NDPOST, nskip = NSKIP, save_trees = TRUE,
-  #                   forests_dir = forests_dir)
 
-  # data_sim <- tmp$df
-  # data_theta <- summarise_draws_3d(x = ml_bart$draws_theta)
-  # data_theta$x <- rep(X[, 1], times = d)
-  # library(ggplot2)
-  # ggplot(data_sim, aes(x = x)) +
-  #   facet_wrap(~category) +
-  #   geom_point(aes(y = prop)) +
-  #   geom_hline(data = data.frame(x = rep(x_true, d), prop = y_ast / sum(y_ast),
-  #                                category = 1:d),
-  #              aes(yintercept = prop), col = "red", linetype = "dashed") +
-  #   # geom_point(data = data.frame(x = rep(x_true, d), prop = y_ast / sum(y_ast),
-  #   #                              category = 1:d),
-  #   #            aes(x = x, y = prop), size = 4, col = "red") +
-  #   geom_line(aes(y = theta), col = "black") +
-  #   geom_line(data = data_theta, aes(x = x, y = median), col = "dodgerblue") +
-  #   geom_ribbon(data = data_theta, aes(x = x, ymin = ci_lower, ymax = ci_upper),
-  #               alpha = 0.5, fill = "dodgerblue")
+  if (file.exists(file.path(path_res, "mod.rds"))) {
+    ml_bart <- load_model(model_dir = path_res)
+  } else {
+    ml_bart <- zanicc(Y = Y_train, X_count = X_train, model = "mult_bart",
+                      ntrees_theta = NTREES, ndpost = NDPOST, nskip = NSKIP,
+                      save_trees = TRUE, forests_dir = forests_dir)
+    save_model(object = ml_bart, model_dir = path_res)
+  }
 
-  # Call the class
-  devtools::load_all()
-  ml <- Rcpp::Module(module = "inverse_posterior", PACKAGE = "zanicc")
-  cpp_obj <- new(ml$InversePosterior, d, NTREES, NTREES, "ml_bart", forests_dir)
+  # Compute the f(x*) and generate proposal
+  N_PROPOSAL <- 2000L
+  proposal_parms <- list(min_x = min(X_train), max_x = max(X_train))
+  compute_proposal_fx_ml_bart(object = ml_bart, proposal_parms = proposal_parms,
+                              n_proposal = N_PROPOSAL, load = FALSE,
+                              save = TRUE, output_dir = path_res)
+  # IS
+  is <- .is_mlbart(object = ml_bart, Y = Y_test, proposal_parms = proposal_parms,
+                   dir_posterior_fx = path_res, n_proposal = N_PROPOSAL)
+  # SIR
+  sir <- .is_mlbart(object = ml_bart, Y = Y_test, proposal_parms = proposal_parms,
+                    dir_posterior_fx = path_res, n_proposal = N_PROPOSAL, sir = TRUE)
 
-  # Sample a case to find its inverse posterior
-  id <- sample(n_sample, 1)
-  (y_ast <- Y[id, ])
-  x_true <- X[id, ]
+  length(sir)
+  dim(is)
 
-  x_ini <- runif(1, min(X), max(X))
-  res <- cpp_obj$SamplerMLBARTeSS(y_ast, as.matrix(x_ini), NDPOST, 0.0,
-                                  as.matrix(1.0), 10)
+  # eSS
+  mean_prior <- 0.0
+  S_prior <- diag(1.0, nrow = 1)
+  ess <- .gibbs_ml_bart(Y = Y_test, mean_prior = mean_prior, S_prior = S_prior,
+                        forests_dir = forests_dir, ntrees = NTREES,
+                        ndpost = NDPOST, n_rep = 2L)
 
-  dim(res)
-  plot(density(res[, 1]) )
-  points(x_true, 0.001, col = "blue", pch = 4, cex = 2)
-  plot(res[, 1], type = "l")
+  # Visual comparison between the three methods
+  x_proposal <- is[, -seq_len(n_test)]
+  pdf(file.path(path_res, "inverse_posterior.pdf"), width = 6, height = 3)
+  for (i in seq_len(n_test)) {
+    x_true <- X_test[i, ]
+    cat(i, "\n")
+    par(mfrow = c(1, 3), mar = c(3, 3, 1, 1))
+    plot(x_proposal, is[, i], type = "S", main = "IS")
+    points(x_true, 0.00001, col = "blue", pch = 4, cex = 2)
+
+    xrange <- range(c(x_true, range(sir[[i]])))
+    plot(density(sir[[i]]), type = "S", main = "SIR", xlim = xrange)
+    points(x_true, 0.00001, col = "blue", pch = 4, cex = 2)
+
+    xrange <- range(c(x_true, range(ess[[i]])))
+    plot(density(ess[[i]]), type = "S", main = "eSS", xlim = xrange)
+    points(x_true, 0.00001, col = "blue", pch = 4, cex = 2)
+  }
+  graphics.off()
 
 })
 
@@ -71,22 +91,32 @@ test_that("inverse posterior ML-BART bivariate X", {
   devtools::load_all()
 
   # Path
-  time_id <- "2026-Apr-13-12:24:25"#format(Sys.time(), "%Y-%b-%d-%X")
-  path_local <- "./tests/testthat/inverse_posterior/ml_bart"
+  time_id <-  "2026-Apr-21-16:00:40" #format(Sys.time(), "%Y-%b-%d-%X")
+  path_local <- "./tests/testthat/inverse_posterior/ml_bart/two_dimension"
+  path_res <- file.path(path_local, time_id, "results")
   forests_dir <- file.path(path_local, time_id, "forests")
   if (!dir.exists(forests_dir)) dir.create(forests_dir, recursive = TRUE)
 
-  n_sample <- 200L
+  set.seed(1212)
   d <- 3L
   n_trials <- 500L
-  tmp <- sim_data_multinomial_2d_covariates(n_grid = 20, d = d, n_trials = n_trials)
-  Y <- tmp$Y
-  X <- tmp$X
-  data_sim <- tmp$df
+  tmp <- sim_data_multinomial_2d(n_grid = 20, d = d, n_trials = n_trials)
 
-  ggplot(data = data.frame(x1 = X[, 1], x2 = X[, 2]),
-         aes(x = x1, y = x2)) +
-    geom_point()
+  # Split the data
+  n_sample <- nrow(tmp$Y)
+  n_test <- 100L
+  id_test <- sample.int(n_sample, n_test)
+  Y_test <- tmp$Y[id_test, ]
+  X_test <- tmp$X[id_test, , drop = FALSE]
+  Y_train <- tmp$Y[-id_test, ]
+  X_train <- tmp$X[-id_test, , drop = FALSE]
+
+  data_sim <- tmp$df[!(tmp$df$id %in% id_test), ]
+  data_sim$id <- rep(seq_len(nrow(Y_train)), each = d)
+
+  # ggplot(data = data.frame(x1 = X[, 1], x2 = X[, 2]),
+  #        aes(x = x1, y = x2)) +
+  #   geom_point()
 
   # ggplot(data = data_sim, aes(x = x1, y = x2, col = theta, fill = theta)) +
   #   facet_wrap(~category) +
@@ -98,73 +128,102 @@ test_that("inverse posterior ML-BART bivariate X", {
   #   )
 
   # Fit forward model
-  NDPOST <- 1000L
-  NSKIP <- 1000L
+  NDPOST <- 5000L
+  NSKIP <- 5000L
   NTREES <- 200L
-  ml_bart <- zanicc(Y = Y, X_count = X, model = "mult_bart", ntrees_theta = NTREES,
-                    ndpost = NDPOST, nskip = NSKIP, save_trees = TRUE,
-                    forests_dir = forests_dir)
-  # data_sim <- tmp$df
-  # data_theta <- summarise_draws_3d(x = ml_bart$draws_theta)
-  # data_theta <- dplyr::left_join(data_theta, data_sim[, c("id", "category", "theta")],
-  #                                by = c("id", "category"))
-  # head(data_theta)
-  # head(data_sim)
-  #
-  # library(ggplot2)
-  # ggplot(data_theta, aes(x = theta, y = median)) +
-  #   facet_wrap(~category) +
-  #   geom_point() +
-  #   geom_abline(slope = 1, intercept = 0)
 
-  # Call the class
-  devtools::load_all()
-  ml <- Rcpp::Module(module = "inverse_posterior", PACKAGE = "zanicc")
-  cpp_obj <- new(ml$InversePosterior, d, NTREES, NTREES, "ml_bart", forests_dir)
+  if (file.exists(file.path(path_res, "mod.rds"))) {
+    ml_bart <- load_model(model_dir = path_res)
+  } else {
+    ml_bart <- zanicc(Y = Y_train, X_count = X_train, model = "mult_bart",
+                      ntrees_theta = NTREES, ndpost = NDPOST, nskip = NSKIP,
+                      save_trees = TRUE, forests_dir = forests_dir)
+    save_model(object = ml_bart, model_dir = path_res)
+  }
 
-  id <- sample(n_sample, 1)
-  (y_ast <- Y[id, ])
-  (x_true <- X[id, ])
+  # Check the model fit
+  data_theta <- summarise_draws_3d(x = ml_bart$draws_theta)
+  data_theta <- dplyr::left_join(data_theta, data_sim[, c("id", "category", "theta")],
+                                 by = c("id", "category"))
+  head(data_theta)
+  head(data_sim)
 
-  p <- ncol(X)
-  S_prior <- cov(X)#diag(1.0, p)
-  x_ini <- x_true
-  res <- cpp_obj$SamplerMLBARTeSS(y_ast, x_ini, NDPOST, rep(0.0, ncol(X)), S_prior, 10)
-  dens <- MASS::kde2d(res[, 1], res[, 2], n = 500)
+  library(ggplot2)
+  ggplot(data_theta, aes(x = theta, y = median)) +
+    facet_wrap(~category) +
+    geom_point() +
+    geom_abline(slope = 1, intercept = 0)
 
-  par(mfrow = c(1, 3))
-  contour(dens$x, dens$y, dens$z, main = "S = I_2")
-  abline(v = x_true[1], h = x_true[2])
-  plot(density(res[, 1]) )
-  points(x_true[1], 0.001, col = "blue", pch = 4, cex = 2)
-  plot(density(res[, 2]) )
-  points(x_true[2], 0.001, col = "blue", pch = 4, cex = 2)
+  # Check the rconvexhull function
+  x_proposal <- rconvexhull(n = 1000, X = X_train)
+  plot(X_train[, 1], X_train[, 2])
+  points(x_proposal[, 1], x_proposal[, 2], col = "blue")
 
-  res2 <- cpp_obj$SamplerMLBARTeSS(y_ast, x_ini, NDPOST, rep(0.0, ncol(X)), S_prior)
-  dens2 <- MASS::kde2d(res2[, 1], res2[, 2], n = 500)
-  contour(dens2$x, dens2$y, dens2$z, main = "S = cov(res)")
-  abline(v = x_true[1], h = x_true[2])
+  # Compute the f(x*) and generate proposal
+  N_PROPOSAL <- 2000L
+  proposal_parms <- list(X = X_train)
+  compute_proposal_fx_ml_bart(object = ml_bart, proposal_parms = proposal_parms,
+                              n_proposal = N_PROPOSAL, load = FALSE,
+                              save = TRUE, output_dir = path_res)
+  # IS
+  is <- .is_mlbart(object = ml_bart, Y = Y_test, proposal_parms = proposal_parms,
+                   dir_posterior_fx = path_res, n_proposal = N_PROPOSAL)
+  # SIR
+  sir <- .is_mlbart(object = ml_bart, Y = Y_test, proposal_parms = proposal_parms,
+                    dir_posterior_fx = path_res, n_proposal = N_PROPOSAL, sir = TRUE)
 
-  dim(res)
-  par(mfrow = c(1, 2))
+  length(sir)
+  dim(is)
+  dim(sir[[1]])
 
-  par(mfrow = c(1, 2))
-  plot(res[, 1], type = "l")
-  plot(res[, 2], type = "l")
+  # eSS
+  mean_prior <- rep(0.0, 2)
+  S_prior <- diag(1.0, nrow = 2)
+  ess <- .gibbs_ml_bart(Y = Y_test, mean_prior = mean_prior, S_prior = S_prior,
+                        forests_dir = forests_dir, ntrees = NTREES,
+                        ndpost = NDPOST, n_rep = 2L)
+  # i=1
+  # Visual comparison between the three methods
+  # x_proposal <- is[, -seq_len(n_test)]
+  pdf(file.path(path_res, "inverse_posterior.pdf"), width = 6, height = 3)
+  for (i in seq_len(n_test)) {
+    x_true <- X_test[i, ]
+    cat(i, "\n")
+    par(mfrow = c(1, 3), mar = c(3, 3, 1, 1))
 
-  head(as.data.frame(res))
+    # plot(x_proposal, is[, i], type = "S", main = "IS")
+    # points(x_true, 0.00001, col = "blue", pch = 4, cex = 2)
 
-  # library(ggplot2)
-  ggplot(as.data.frame(res), aes(x=V1, y=V2)) +
-    geom_density2d() +
-    geom_vline(xintercept = x_true[1]) +
-    geom_hline(yintercept = x_true[2])
-  ggplot(as.data.frame(res2), aes(x=V1, y=V2)) +
-    geom_density2d() +
-    geom_vline(xintercept = x_true[1]) +
-    geom_hline(yintercept = x_true[2])
+    # Linear interpolation for IS
+    is_interp <- interp(x_proposal[, 1], x_proposal[, 2], is[, i], linear = TRUE)
 
+    # Compute the KDE for SIR and eSS
+    dens_sir <- MASS::kde2d(sir[[i]][, 1], sir[[i]][, 2])
+    dens_ess <- MASS::kde2d(ess[[i]][, 1], ess[[i]][, 2])
+    # Common limits
+    xrange <- range(c(x_true[1], range(dens_sir$x), range(dens_ess$x), range(is_interp$x)))
+    yrange <- range(c(x_true[2], range(dens_sir$y), range(dens_ess$y), range(is_interp$x)))
 
+    # dens_sir <- MASSExtra::kde_2d(ess[[i]][, 1], ess[[i]][, 2], kernel = "opt")
+    # dens_ess <- MASSExtra::kde_2d(ess[[i]][, 1], ess[[i]][, 2], kernel = "opt")
 
+    contour(is_interp$x, is_interp$y, is_interp$z, main = "IS", ylim = yrange,
+            xlim = xrange)
+    points(x_true[1], x_true[2], col = "blue", pch = 4, cex = 2)
+    abline(v = x_true[1], h = x_true[2])
+
+    # SIR
+    contour(dens_sir$x, dens_sir$y, dens_sir$z, main = "SIR", ylim = yrange,
+            xlim = xrange)
+    points(x_true[1], x_true[2], col = "blue", pch = 4, cex = 2)
+    abline(v = x_true[1], h = x_true[2])
+
+    # eSS
+    contour(dens_ess$x, dens_ess$y, dens_ess$z, main = "eSS", ylim = yrange,
+            xlim = xrange)
+    points(x_true[1], x_true[2], col = "blue", pch = 4, cex = 2)
+    abline(v = x_true[1], h = x_true[2])
+  }
+  graphics.off()
 
 })
